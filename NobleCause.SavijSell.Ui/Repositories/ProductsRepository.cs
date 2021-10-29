0d543912588bbb57c16f1698834bee64af578c63
jeff.noble <savijcoder@outlook.com>
2021-10-29 13:17:29 -0400

    * added bearer token (hard coded)

--- NobleCause.SavijSell.Ui/Repositories/ProductsRepository.cs
+++ NobleCause.SavijSell.Ui/Repositories/ProductsRepository.cs
@@ -15,11 +15,8 @@ namespace NobleCause.SavijSell.Ui.Repositories
 
             return await "https://localhost:44328"
                          .AppendPathSegment("/api/Products")
+                         .WithOAuthBearerToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwianRpIjoiOWQzYWU0YWQtNjI4ZS00OWIyLWFiMDAtMGUyY2I0NmNkMjkyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IlNhdmlqQ29kZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJzYXZpai5jb2RlckBzaGFya2xhc2Vycy5jb20iLCJleHAiOjE2MzQ2OTcwODQsImlzcyI6ImxvY2FsaG9zdDo0NDMyOCIsImF1ZCI6ImxvY2FsaG9zdDo0NDMyOCJ9.l-AENf9_gQi1bDwTT9sCfRica5TNvADj8bv9512z8Bo")
                          .GetJsonAsync<List<Product>>();
-
-            //var httpClient = new HttpClient();
-            //var products = await httpClient.GetFromJsonAsync<List<Product>>("https://localhost:44328/api/Products");
-            //return products;
         }
 
         public async Task<Product> GetProductsById(int id)
